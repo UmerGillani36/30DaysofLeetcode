05677d156ea7ff595b5e44db6dc4574ffb9a222f
Syed Umer Shah Gillani <syedomershahgillani@gmail.com>
2024-03-12 19:28:26 +0500

    Time: 66 ms (88.40%), Space: 58.6 MB (78.32%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+     if (prices.length === 0) return 0;
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+
+    return maxProfit;
+};
