ed1ec8dd45bb49492802525c83b39bd3ca272cb2
Syed Umer Shah Gillani <syedomershahgillani@gmail.com>
2024-03-13 17:14:36 +0500

    Time: 272 ms (8.85%), Space: 51.3 MB (59.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,9 +3,7 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    
-    const sortedNums = nums.sort((a,b)=> a - b);
-    
+        
     for(let i=0; i<= nums.length; i++){
             if(!nums.includes(i)){
                return i;
